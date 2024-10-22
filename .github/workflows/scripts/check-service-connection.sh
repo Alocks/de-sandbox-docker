73a7fd371c8c4d4ab2f4fe3077ef7c3af9deee21
Alocks <alocksmasao@gmail.com>
2024-10-22 05:37:13 +0000

    fix CI yet again

--- .github/workflows/scripts/check-service-connection.sh
+++ .github/workflows/scripts/check-service-connection.sh
@@ -1,8 +1,10 @@
 function check_ip_connection {
-    host="$1" && port="$2" && counter=0 && success=false
+    host="$1" && port="$2" && counter=0 && success=false && max_attempts=5
 
-    while [[ $counter -lt 5 && $success == false ]]; do
-        sleep 1 && counter=$((counter + 1))
+    while [[ $counter -lt $max_attempts && $success == false ]]; do
+        counter=$((counter + 1))
+        echo "Checking connection for $host:$port... $counter out of $max_attempts tries"
+        sleep 1
         conn_message=$(nc -vz $host $port 2>&1 | awk '{print $NF}')
         if [[ "$conn_message" == "succeeded!" ]]; then
             success=true
@@ -15,11 +17,10 @@ host='localhost'
 ports_var=$(yq -r '.services[] | .ports[] | split(":")[0]' 'compose.yml')
 for port_var in $ports_var; do
     port_num=$(eval echo $port_var)
-    echo 1
     if [[ -z "$port_num" ]]; then
         echo "::error ${port_var} IS NOT SET! Configure environment variables to fix this issue." && exit 1
     fi
-    conn_result=$(check_ip_connection $port_num)
+    conn_result=$(check_ip_connection $host $port_num)
     if [[ $conn_result == false ]]; then
         echo "::error Failed to connect to $host:$port_num. Check the compose or image file." && exit 1
     fi
